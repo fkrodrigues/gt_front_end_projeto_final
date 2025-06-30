42d392e82324f33eecf9ae81bc12a2566f1daf2a
Frank Rodrigues <fkrl.rock@gmail.com>
2025-06-30 15:15:41 -0300

    Updates

---
+++ assets/ProductListingPage-C3Xzom0U.js
@@ -0,0 +1,73 @@
+import{j as e,G as c,r as l}from"./index-CEo9E97U.js";import{P as m,t as s}from"./tenisAlta-BeHOfcbo.js";const x=({checked:n,radio:a,text:i,onChange:t})=>e.jsx("div",{children:e.jsxs("label",{className:` h-[26px] flex \r
+           gap-2 text-[#474747] \r
+         `,children:[e.jsx("input",{type:a?"radio":"checkbox",checked:n,onChange:t,className:`\r
+          accent-[#C92071] h-[22px] w-[22px]\r
+          \r
+          \r
+          `}),i]})}),o=({inputType:n,title:a})=>e.jsx(e.Fragment,{children:e.jsxs("div",{className:"mt-5 ml-5",children:[e.jsx("h4",{className:"mb-2",children:a}),n.map((i,t)=>e.jsx(x,{checked:i.checked,radio:i.radio,text:i.text,onChange:i.onChange},t))]})}),d=({arrayFilters:n})=>e.jsx(e.Fragment,{children:e.jsx("div",{className:"text-[#474747] ",children:n.map((a,i)=>e.jsx(o,{inputType:a.inputType,title:a.title},i))})});function p(n){return c({attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{id:"Filter"},child:[{tag:"path",attr:{d:"M14.037,20.937a1.015,1.015,0,0,1-.518-.145l-3.334-2a2.551,2.551,0,0,1-1.233-2.176V12.091a1.526,1.526,0,0,0-.284-.891L4.013,4.658a1.01,1.01,0,0,1,.822-1.6h14.33a1.009,1.009,0,0,1,.822,1.6h0L15.332,11.2a1.527,1.527,0,0,0-.285.891v7.834a1.013,1.013,0,0,1-1.01,1.012ZM4.835,4.063,9.482,10.62a2.515,2.515,0,0,1,.47,1.471v4.524a1.543,1.543,0,0,0,.747,1.318l3.334,2,.014-7.843a2.516,2.516,0,0,1,.471-1.471l4.654-6.542,0,0Z"},child:[]}]}]})(n)}const F=()=>{const n=[{title:"Marca",inputType:[{checked:!0,text:"Adidas"},{checked:!1,text:"Calenciaga"},{checked:!0,text:"K-Swiss"},{checked:!1,text:"Nike"},{checked:!1,text:"Puma"}]},{title:"Categoria",inputType:[{checked:!0,text:"Esporte e Lazer"},{checked:!1,text:"Casual"},{checked:!1,text:"Utilitário"},{checked:!1,text:"Corrida"}]},{title:"Gênero",inputType:[{checked:!0,text:"Masculino"},{checked:!0,text:"Feminino"},{checked:!1,text:"Unissex"}]},{title:"Estado",inputType:[{radio:!0,checked:!0,text:"Novo"},{radio:!0,checked:!1,text:"Usado"}]}],a=[{discount:"30% OFF",image:s,name:"K-Swiss V8 - Masculino",type:"Tênis",price:"$200",priceDiscount:"$100"},{discount:"30% OFF",image:s,name:"K-Swiss V8 - Masculino",type:"Tênis",price:"$200",priceDiscount:"$100"},{discount:"30% OFF",image:s,name:"K-Swiss V8 - Masculino",type:"Tênis",price:"$200",priceDiscount:"$100"},{discount:"30% OFF",image:s,name:"K-Swiss V8 - Masculino",type:"Tênis",price:"$200",priceDiscount:"$100"},{discount:"30% OFF",image:s,name:"K-Swiss V8 - Masculino",type:"Tênis",price:"$200",priceDiscount:"$100"},{discount:"30% OFF",image:s,name:"K-Swiss V8 - Masculino",type:"Tênis",price:"$200",priceDiscount:"$100"},{discount:"30% OFF",image:s,name:"K-Swiss V8 - Masculino",type:"Tênis",price:"$200",priceDiscount:"$100"},{discount:"30% OFF",image:s,name:"K-Swiss V8 - Masculino",type:"Tênis",price:"$200",priceDiscount:"$100"},{discount:"30% OFF",image:s,name:"K-Swiss V8 - Masculino",type:"Tênis",price:"$200",priceDiscount:"$100"},{discount:"30% OFF",image:s,name:"K-Swiss V8 - Masculino",type:"Tênis",price:"$200",priceDiscount:"$100"},{discount:"30% OFF",image:s,name:"K-Swiss V8 - Masculino",type:"Tênis",price:"$200",priceDiscount:"$100"},{discount:"30% OFF",image:s,name:"K-Swiss V8 - Masculino",type:"Tênis",price:"$200",priceDiscount:"$100"},{discount:"30% OFF",image:s,name:"K-Swiss V8 - Masculino",type:"Tênis",price:"$200",priceDiscount:"$100"},{discount:"30% OFF",image:s,name:"K-Swiss V8 - Masculino",type:"Tênis",price:"$200",priceDiscount:"$100"},{discount:"30% OFF",image:s,name:"K-Swiss V8 - Masculino",type:"Tênis",price:"$200",priceDiscount:"$100"}],[i,t]=l.useState(!1),r=()=>{t(!i)};return e.jsxs("div",{className:` \r
+           flex flex-col items-center\r
+           h-[2600px] \r
+           max-sm:relative\r
+           max-sm:w-[375px]\r
+           `,children:[e.jsxs("div",{className:`\r
+            flex justify-between \r
+            w-[1300px] my-10\r
+            max-sm:w-[100%]\r
+            max-sm:flex-col\r
+            max-sm:my-0\r
+            max-sm:p-5\r
+            max-sm:relative\r
+            max-sm:h-[160px]\r
+          `,children:[e.jsxs("div",{className:`\r
+              flex\r
+              max-sm:my-8\r
+              max-sm:translate-y-15\r
+            `,children:[e.jsx("h4",{children:'Resultado para "Tênis"'}),e.jsx("p",{children:" - 389 produtos"})]}),e.jsxs("div",{className:`\r
+              flex gap-1 border-1 p-3\r
+              rounded-lg\r
+              max-sm:p-5\r
+              max-sm:-translate-y-20\r
+              max-sm:w-[20em]\r
+              max-sm:text-[80%]\r
+\r
+              `,children:[e.jsx("h4",{children:e.jsx("label",{htmlFor:"ordenar",children:"Ordenar por:"})}),e.jsxs("select",{id:"ordenar",children:[e.jsx("option",{value:"op1",children:"mais relevantes"}),e.jsx("option",{value:"op2",children:"maior preço"}),e.jsx("option",{value:"op3",children:"menor preço"})]})]}),e.jsx("div",{className:`
+            hidden max-sm:flex 
+            w-[59px] h-[59px]
+            bg-[#C92071]
+            rounded-lg
+            justify-center items-center
+            text-white text-4xl
+            absolute
+            right-5
+            top-7
+            active:bg-[#77044d]
+          `,children:e.jsx(p,{onClick:r})})]}),e.jsxs("div",{className:`\r
+           flex w-[1300px] relative \r
+           max-sm:w-full \r
+           `,children:[e.jsx("div",{onClick:r,className:`
+               ${i?"max-sm:flex":"max-sm:hidden"}
+                flex flex-wrap w-[1300px]
+                h-[720px] relative
+                max-sm:w-[100%]
+                max-sm:absolute
+                max-sm:top-0
+                max-sm:bg-[#47474793]
+                max-sm:h-[170em]
+                max-sm:z-3
+             `,children:e.jsxs("div",{className:`\r
+                  bg-white\r
+                  w-[280px]\r
+                  text-[#474747]\r
+                  max-sm:h-[700px]\r
+                  \r
+                  `,children:[e.jsx("h4",{className:"my-5 ml-5  w-30",children:"Filtrar por"}),e.jsx("hr",{className:`\r
+                    text-[#CCCCCC] w-[220px]\r
+                     ml-5\r
+                    `}),e.jsx(d,{arrayFilters:n})]})}),e.jsx("div",{className:`\r
+               w-[80%] flex ml-8\r
+               max-sm:ml-0\r
+               max-sm:w-[375px]\r
+               max-sm:h-[2370px]\r
+               max-sm:flex-col\r
+               max-sm:overflow-hidden\r
+               `,children:e.jsx(m,{arrayProductCard:a})})]})]})};export{F as default};
--- assets/tenisAlta-BeHOfcbo.js
+++ assets/tenisAlta-BeHOfcbo.js
@@ -1,11 +1,11 @@
-import{j as r,L as c}from"./index-BzjXSx9L.js";const i=({discount:e,image:n,type:s,name:t,price:x,priceDiscount:a})=>r.jsx(c,{to:"/products/1",children:r.jsx("div",{className:`\r
+import{j as n,L as i}from"./index-CEo9E97U.js";const c=({discount:e,image:r,type:s,name:t,price:x,priceDiscount:a})=>n.jsx(i,{to:"/products/1",children:n.jsx("div",{className:`\r
           w-[300px] h-[439px]\r
           max-sm:w-[100%]\r
         \r
-        `,children:r.jsxs("div",{className:`relative\r
+        `,children:n.jsxs("div",{className:`relative\r
              bg-white shadow-sm rounded-[5px]\r
                w-[315px] h-[347px]  \r
-             `,children:[r.jsx("div",{className:`\r
+             `,children:[n.jsx("div",{className:`\r
                     absolute\r
                     bg-[#E7FF86]\r
                     rounded-[29px]\r
@@ -15,31 +15,31 @@ import{j as r,L as c}from"./index-BzjXSx9L.js";const i=({discount:e,image:n,type
                     text-[#474747]\r
                     flex justify-center \r
                     items-center\r
-                `,children:e}),r.jsx("div",{className:`\r
+                `,children:e}),n.jsx("div",{className:`\r
                     flex justify-center items-center\r
                     h-full\r
-                    `,children:r.jsx("img",{className:`\r
+                    `,children:n.jsx("img",{className:`\r
                      -rotate-30 rounded-[10px]\r
-                     `,src:n,alt:"Tênis"})}),r.jsx("div",{className:`\r
+                     `,src:r,alt:"Tênis"})}),n.jsx("div",{className:`\r
                   text-[14px] font-bold\r
                    text-[#8F8F8F] my-2\r
                    tracking-[.75px]\r
-                `,children:s}),r.jsx("div",{className:`\r
+                `,children:s}),n.jsx("div",{className:`\r
                   text-[#474747] text-[26px]\r
-                `,children:t}),r.jsxs("div",{className:"flex gap-3",children:[r.jsx("div",{className:`\r
+                `,children:t}),n.jsxs("div",{className:"flex gap-3",children:[n.jsx("div",{className:`\r
                   text-[#8F8F8F] text-[24px]\r
                     line-through decoration-[1.5px]\r
-                `,children:x}),r.jsx("div",{className:`\r
+                `,children:x}),n.jsx("div",{className:`\r
                   text-[#1F1F1F] text-[24px]\r
                   font-bold \r
 \r
-                `,children:a})]})]})})}),l=({arrayProductCard:e})=>r.jsx("div",{className:` \r
+                `,children:a})]})]})})}),d=({arrayProductCard:e})=>n.jsx("div",{className:` \r
           text-[#474747] \r
           mb-4 \r
           w-[1320px]\r
           max-sm:ml-0\r
           max-sm:w-[23.4rem]\r
-        `,children:r.jsx("div",{className:`\r
+        `,children:n.jsx("div",{className:`\r
             flex flex-wrap gap-9\r
             max-sm:justify-center\r
-          `,children:e.map((n,s)=>r.jsx(i,{discount:n.discount,image:n.image,type:n.type,name:n.name,price:n.price,priceDiscount:n.priceDiscount},s))})});export{l as P};
+          `,children:e.map((r,s)=>n.jsx(c,{discount:r.discount,image:r.image,type:r.type,name:r.name,price:r.price,priceDiscount:r.priceDiscount},s))})}),p="/gt_front_end_projeto_final/assets/tenisAlta-CNV6_LSw.png";export{d as P,p as t};
